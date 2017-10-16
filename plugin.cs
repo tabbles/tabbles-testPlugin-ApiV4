d75d38cdda919ecd14b123d3ed94154f9073df78
Andrea-Tabbles <andrea@tabbles.net>
2017-10-16 18:10:22 +0200

    Initial commit

---
+++ plugin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaggerApi;
+
+namespace testPlugin
+{
+    public class MyPlugin : TaggerApi.IPlugin, TaggerApi.IEventListener, TaggerApi.IMainMenuExtender, TaggerApi.IInitializable
+    {
+        myWindow mWindow;
+
+        public string Name => "My test plugin";
+
+        public string PluginVersion => "1.0";
+
+
+
+        public string Author => "Author name";
+
+        public void Initialize()
+        {
+            mWindow = new myWindow();
+        }
+
+        public IEnumerable<MenuItem> menuItems()
+        {
+            var mi = new MenuItem
+            {
+                Name = "Open plugin UI",
+                Tooltip = "opens the UI of this plugin",
+                onItemClicked = () =>
+                {
+                    mWindow.Show();
+                }
+            };
+
+            return new[] { mi };
+
+        }
+
+        public void onFilesTagged(IEnumerable<file> files, IEnumerable<tag> tags)
+        {
+            var strTags = tags.Select(t => t.name).Aggregate((t1, t2) => t1 + ", " + t2);
+            var strFiles = files.Select(f => f.path).Aggregate((f1, f2) => f1 + Environment.NewLine + f2);
+            mWindow.txtLog.AppendText("files tagged with these tags: " + strTags + Environment.NewLine +  strFiles);
+            mWindow.txtLog.AppendText(Environment.NewLine);
+            mWindow.txtLog.AppendText(Environment.NewLine);
+        }
+
+        public void onFilesUntagged(IEnumerable<file> files, IEnumerable<tag> tags)
+        {
+            var strTags = tags.Select(t => t.name).Aggregate((t1, t2) => t1 + ", " + t2);
+            var strFiles = files.Select(f => f.path).Aggregate((f1, f2) => f1 + Environment.NewLine + f2);
+            mWindow.txtLog.AppendText("files untagged from these tags: " + strTags + Environment.NewLine + strFiles);
+            mWindow.txtLog.AppendText(Environment.NewLine);
+            mWindow.txtLog.AppendText(Environment.NewLine);
+        }
+
+        public void onTagCreated(tag tag)
+        {
+            mWindow.txtLog.AppendText("tag created: " + tag.name);
+            mWindow.txtLog.AppendText(Environment.NewLine);
+            mWindow.txtLog.AppendText(Environment.NewLine);
+        }
+
+        public void onTagsDeleted(IEnumerable<tag> tags)
+        {
+            var strTags = tags.Select(t => t.name).Aggregate((t1, t2) => t1 + ", " + t2);
+            mWindow.txtLog.AppendText("tags deleted: " + strTags);
+            mWindow.txtLog.AppendText(Environment.NewLine);
+            mWindow.txtLog.AppendText(Environment.NewLine);
+
+        }
+    }
+}
---
+++ pluginWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testPlugin
+{
+    /// <summary>
+    /// Interaction logic for window.xaml
+    /// </summary>
+    public partial class myWindow : Window
+    {
+        public myWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnTag_Click(object sender, RoutedEventArgs e)
+        {
+            var tagName = txtTagName.Text.Trim();
+
+            // first you need to create the tag, otherwise the tagFiles call will do nothing
+            var createTagRes = await Api.createTagWithDefaultColor(tagName);
+
+
+            var filesToTag = new[] { txtFilePath.Text.Trim() };
+            var tags = new[] { tagName };
+
+            var tagRes = await Api.tagFiles(tags: tags, files: filesToTag);
+
+
+        }
+
+        private async void btnUntag_Click(object sender, RoutedEventArgs e)
+        {
+            var tagName = txtTagName.Text.Trim();
+
+            // here you don't need to create the tag. if the tag does not exist, the untag operation does not need to do anything.
+            
+
+            var filesToTag = new[] { txtFilePath.Text.Trim() };
+            var tags = new[] { tagName };
+
+            var tagRes = await Api.untagFiles(tags: tags, files: filesToTag);
+             
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            var y = 4;
+        }
+    }
+}
